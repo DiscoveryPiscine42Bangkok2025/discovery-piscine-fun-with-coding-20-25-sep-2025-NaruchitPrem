8053463de5ccf59677c27405f816060593605f04
NaruchitPrem <naruchitprem@gmail.com>
2025-09-21 16:45:33 +0700

    cell03-ex00

---
+++ cell03/ex00/background.js
@@ -0,0 +1,17 @@
+const changeButton = document.getElementById('changeBtn');
+
+function getRandomHexColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+changeButton.addEventListener('click', function() {
+
+    const randomColor = getRandomHexColor();
+
+    document.body.style.backgroundColor = randomColor;
+});
